ddaf68dfe2240132763203665a673272bc4ba622
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-21 03:03:13 +0000

    Deploy denverdevs.org to github.com/Denver-Devs/denverdevs.org.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,15 +21,15 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "10ef6ed47e49576a72a632c47c742c9e"
+    "revision": "b448b101080284e608ac06291da1035d"
   },
   {
     "url": "about/index.html",
-    "revision": "5f2cad13f2d322cf3d579e35efbfd954"
+    "revision": "8901a7f500d7ce00eda9c69fc23c92ba"
   },
   {
-    "url": "assets/css/0.styles.c959dc58.css",
-    "revision": "7063a37aa97d6e3d98c982885381e422"
+    "url": "assets/css/0.styles.c7acd22b.css",
+    "revision": "024f1b5172a1c80cf277d2d465afd05b"
   },
   {
     "url": "assets/img/search.83621669.svg",
@@ -52,8 +52,8 @@ self.__precacheManifest = [
     "revision": "89dfbdf9490711d964032c33eed0eaad"
   },
   {
-    "url": "assets/js/13.115e3fea.js",
-    "revision": "b1f95911d1097efe3b34b54a68c4dd68"
+    "url": "assets/js/13.d4113bab.js",
+    "revision": "76bf1079d3d04e732a53086e5a9a71eb"
   },
   {
     "url": "assets/js/2.aaea46d0.js",
@@ -88,12 +88,12 @@ self.__precacheManifest = [
     "revision": "ab1d1ee67bdbd08f94755181ce3f678c"
   },
   {
-    "url": "assets/js/app.7f87616e.js",
-    "revision": "ec0f7465d1c0da63ad134ae262365152"
+    "url": "assets/js/app.d01e857e.js",
+    "revision": "4ba8978a9488ac9c90df59bd608f0dec"
   },
   {
     "url": "channel-guide/index.html",
-    "revision": "e89948cfb819e6fe8c968390f0f59e73"
+    "revision": "f057e2c02756340c9b7f38550a123923"
   },
   {
     "url": "icon.png",
@@ -101,31 +101,31 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "68c14d421e1300e5fe93bc81783abc16"
+    "revision": "47c87a389ecbff874f39a68acdf98508"
   },
   {
     "url": "resources/code-of-conduct/index.html",
-    "revision": "2401889a25f9d1c6aebc3a02868433b4"
+    "revision": "3940db531de97984c5d7c2699f5d88ee"
   },
   {
     "url": "resources/gig-post-formatting/index.html",
-    "revision": "672c420627e4377fca9720a57dab53a7"
+    "revision": "f25649f5ff70c39f859fc84f7e0968ae"
   },
   {
     "url": "resources/index.html",
-    "revision": "9f3b4f06d178fe2557d029e818816f52"
+    "revision": "62cdeb0a7563413866e7f880bbb44e80"
   },
   {
     "url": "resources/recruiter-guidelines/index.html",
-    "revision": "925f8e5bd9ae9a42f9fa3a4dc05d655f"
+    "revision": "a25b6c4026e8148113663fa51ee98014"
   },
   {
     "url": "resources/rules/index.html",
-    "revision": "38718ff3cbaaa4146e6d8e30ab327dd1"
+    "revision": "ff87c91c3d13293bbda3ff8a2cf3785e"
   },
   {
     "url": "resources/welcome-to-denver/index.html",
-    "revision": "a3d448344793589b44f720754663623a"
+    "revision": "b325acf54fc6e70943c7a058175d5476"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
